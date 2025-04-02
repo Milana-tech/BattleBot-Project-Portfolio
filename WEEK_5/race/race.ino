5b8ceeac066820610d2dda0aa089cce2360acc04
Milana <milana.doborjginidze@student.nhlstenden.com>
2025-04-02 12:31:02 +0200

    Code with Object Avoidance and Line Following.

--- WEEK_5/race/race.ino
+++ WEEK_5/race/race.ino
@@ -1,130 +1,103 @@
 // гриппер не работает, линию теряет. Заставить cone работать и робота следовать линии с object avoidance
 
+// линия работает. Импелентировать стоп на черном квадрате.
 // Line sensors
-#define NUM_SENSORS 8  
-int _sensorPins[NUM_SENSORS] = {A0, A1, A2, A3, A4, A5, A6, A7};
-int _sensorValues[NUM_SENSORS];
-bool _whereIsLine[NUM_SENSORS];  
-float _black = 830;
+#define NUM_SENSORS 8
+int _sensorPins[NUM_SENSORS] = { A0, A1, A2, A3, A4, A5, A6, A7 };
 
 // Motor pins
-#define MA1 11 //RF
-#define MA2 10 //RB
-#define MB1 9 //RB
-#define MB2 5 //LF
+#define MA1 11  //RF
+#define MA2 10  //RB
+#define MB1 9   //RB
+#define MB2 5   //LF
 
 #define TRIG 4
 #define ECHO 3
 
-#define SERVO 6           // Pin connected to the servo motor (PWM pin)
-#define GRIPPER_OPEN 1500   
+#define SERVO 6  // Pin connected to the servo motor (PWM pin)
+#define GRIPPER_OPEN 1500
 #define GRIPPER_CLOSE 1050
 
-#define SERVO_UPDATE_INTERVAL 20 // Servo update interval (ms)
+#define SERVO_UPDATE_INTERVAL 20  // Servo update interval (ms)
 
 // variables
 const int _sensorDataInterval = 500;
 int _timerColor = 0;
-const float WHEEL_DIAMETER = 6.5;  
+const float WHEEL_DIAMETER = 6.5;
 const float _MOTOR_SPEED_CM_PER_SEC = 20.0;
+int _sensorValues[NUM_SENSORS];
+bool _whereIsLine[NUM_SENSORS];
+float _black = 830;
+static unsigned long lastUpdate = 0; 
+unsigned long startCheckTime = 0;  // Время начала проверки черного квадрата
+bool isOnBlackSquare = false;
 
-// bool detectBlackSquare()
-// {
-//   for (int i = 0; i < NUM_SENSORS; i++)
-//   {
-//       if (!_whereIsLine[i])
-//       {
-//           return false;
-//       }
-//   }
+int front = -100;
 
-//   return true; 
-// }
 
 // Motor control functions
-void rightStop()
-{
+void rightStop() {
   analogWrite(MB1, LOW);
   analogWrite(MB2, LOW);
 }
 
-void leftStop()
-{
+void leftStop() {
   analogWrite(MA1, LOW);
   analogWrite(MA2, LOW);
 }
 
-void allStop()
-{
+void allStop() {
   leftStop();
   rightStop();
 }
 
-void leftForward(int speed)
-{
+void leftForward(int speed) {
   analogWrite(MA2, speed);
   analogWrite(MA1, 0);
 }
 
-void rightForward(int speed)
-{
+void rightForward(int speed) {
   analogWrite(MB1, speed);
   analogWrite(MB2, 0);
 }
 
-void rightBack(int speed)
-{
+void rightBack(int speed) {
   analogWrite(MB2, speed);
   analogWrite(MB1, 0);
 }
 
-void leftBack(int speed)
-{
+void leftBack(int speed) {
   analogWrite(MA1, speed);
   analogWrite(MA2, 0);
 }
 
-void setRightMotor(int speed)
-{
-  if (speed > 0)
-  {
+void setRightMotor(int speed) {
+  if (speed > 0) {
     rightForward(speed);
-  }
-  else if (speed < 0)
-  {
+  } else if (speed < 0) {
     rightBack(speed * -1);
-  }
-  else
-  {
+  } else {
     rightStop();
   }
 }
 
-void setLeftMotor(int speed)
-{
-  if (speed > 0)
-  {
+void setLeftMotor(int speed) {
+  if (speed > 0) {
     leftForward(speed);
-  }
-  else if (speed < 0)
-  {
+  } else if (speed < 0) {
     speed = speed * -1;
     leftBack(speed);
-  }
-  else
-  {
+  } else {
     leftStop();
   }
 }
 
-void setBothMotor(int speed)
-{
+void setBothMotor(int speed) {
   setLeftMotor(speed);
   setRightMotor(speed);
 }
 
-void setMotors(int speed1, int speed2)
-{
+void setMotors(int speed1, int speed2) {
   setLeftMotor(speed1);
   setRightMotor(speed2);
 }
@@ -138,18 +111,14 @@ void calibrate()
   Serial.println(_black);
 }
 
-void read_color()
-{
-  for (int i = 0; i < NUM_SENSORS; i++)
-  {
+void read_color() {
+  for (int i = 0; i < NUM_SENSORS; i++) {
     _sensorValues[i] = analogRead(_sensorPins[i]);
   }
 }
 
-void read_bool_color()
-{
-  for (int i = 0; i < NUM_SENSORS; i++)
-  {
+void read_bool_color() {
+  for (int i = 0; i < NUM_SENSORS; i++) {
     _whereIsLine[i] = analogRead(_sensorPins[i]) > _black;
   }
 }
@@ -174,22 +143,19 @@ void avoidObject()
   }
   setMotors(255, -255);
   delay(200);
- 
 }
 
 void keepGripperClosed()
 {
-  static unsigned long lastUpdate = 0;
-  if (lastUpdate - millis() >= SERVO_UPDATE_INTERVAL)
-  {
-      digitalWrite(SERVO, HIGH);
-      delayMicroseconds(GRIPPER_CLOSE);
-      digitalWrite(SERVO, LOW);
-      lastUpdate = millis();
+  if (lastUpdate - millis() >= SERVO_UPDATE_INTERVAL) {
+    digitalWrite(SERVO, HIGH);
+    delayMicroseconds(GRIPPER_CLOSE);
+    digitalWrite(SERVO, LOW);
+    lastUpdate = millis();
   }
 }
 
-float getDistance(int trig=TRIG, int echo=ECHO)
+float getDistance(int trig = TRIG, int echo = ECHO)
 {
   digitalWrite(trig, LOW);
   delayMicroseconds(2);
@@ -197,68 +163,98 @@ float getDistance(int trig=TRIG, int echo=ECHO)
   delayMicroseconds(10);
   digitalWrite(trig, LOW);
 
-  long duration = pulseIn(echo, HIGH);
-  float distance = duration * 0.034 / 2; 
+  long duration = pulseIn(echo, HIGH, 30000); // Таймаут 30ms (предотвращает зависание)
+
+  if (duration == 0)
+  {
+    return 29999; // Если нет отклика, возвращаем -1
+  }
+
+  float distance = duration * 0.034 / 2;
 
   return distance;
+}
+
+bool detectBlackSquare()
+{
+  read_bool_color(); 
+
+  for (int i = 0; i < NUM_SENSORS; i++)
+  {
+    if (!_whereIsLine[i])
+    { 
+      return false;
+    } 
+  }
+  return true;
 }
 
 void followLine()
 {
   read_bool_color();
 
-  // не работает потому что робот изначально на черном квадрате и не поедет вперед даже после того как флаг поднимется
-  
-  // if (detectBlackSquare())
+  // Если это первый запуск, запоминаем момент старта
+  // if (startCheckTime == 0)
   // {
-  //   allStop();
-  //   while (true)
-  //   {
-  //     delay(1000);
-  //   }
-
-    // add go backwards
+  //   startCheckTime = millis();
   // }
-  keepGripperClosed();
 
+  // if (millis() - startCheckTime > 5000)
+  // {
+  //   if (detectBlackSquare())
+  //   {
+  //     setMotors(255, 255);  // Робот двигается немного вперед
+  //     delay(500);  // Ждем немного, чтобы проехать
+
+  //     read_bool_color();  // Считываем значения с датчиков
+
+  //     if (detectBlackSquare()) 
+  //     {
+  //       allStop();  // Останавливаем робота
+  //       while(true)
+  //       {
+  //         delay(500);  // Бесконечный цикл, робот не двигается
+  //       }
+  //     }
+  //   }
+  // }
   if (getDistance() < 15)
   {
     avoidObject();
-  }
+  } 
   else if (_whereIsLine[3] || _whereIsLine[4])
-  { // line is in the middle
+  {  // line is in the middle
     setMotors(255, 255);
   }
   else if (_whereIsLine[0])
-  { // Line is far left
+  {  // Line is far left
     setMotors(255, 0);
   }
-    else if (_whereIsLine[7])
-  { // Line is far right
+  else if (_whereIsLine[7])
+  {  // Line is far right
     setMotors(0, 255);
   }
-  else if (_whereIsLine[5]) //откалибровать для каждого сенсора свое значение для скорости и плавности 
-  { // Line is slightly to the right
+  else if (_whereIsLine[5])  //откалибровать для каждого сенсора свое значение для скорости и плавности
+  {                            // Line is slightly to the right
     setMotors(175, 255);
   }
   else if (_whereIsLine[6])
   {
     setMotors(175, 255);
   }
-  else if ( _whereIsLine[2])
+  else if (_whereIsLine[2])
   {
     setMotors(255, 175);
   }
   else if (_whereIsLine[1])
-  { // Line is slightly to the left
+  {  // Line is slightly to the left
     setMotors(255, 175);
   }
-  
 }
 
 void gripper(int pulse, int count)
 {
-  while(count < 11 )
+  while (count < 11)
   {
     static unsigned long timer;
     if (millis() > timer)
@@ -266,13 +262,13 @@ void gripper(int pulse, int count)
       digitalWrite(SERVO, HIGH);
       delayMicroseconds(pulse);
       digitalWrite(SERVO, LOW);
-      timer = millis() + 20; // 20 ms update interval
+      timer = millis() + 20;  // 20 ms update interval
       count++;
     }
   }
 }
 
-void start() 
+void start()
 {
   while (getDistance() > 15)
   {
@@ -280,23 +276,23 @@ void start()
     delay(50);
   }
 
-  while (getDistance() < 15) 
+  while (getDistance() < 15)
   {
     allStop();
     delay(50);
   }
 
-  setMotors(255, 255); 
+  setMotors(255, 255);
   delay(600);
   gripper(GRIPPER_CLOSE, 3);
   setMotors(0, 255);
+  delay(400);
   read_bool_color();
   while (!((_whereIsLine[3] || _whereIsLine[4] || _whereIsLine[2] || _whereIsLine[5]) && !_whereIsLine[7] && !_whereIsLine[0]))
   {
     delay(10);
-    read_bool_color();      
+    read_bool_color();
   }
-  allStop();
 }
 
 void stop()
@@ -304,8 +300,7 @@ void stop()
   gripper(GRIPPER_OPEN, 3);
 }
 
-void setup()
-{
+void setup() {
   // motor as outputs
   Serial.begin(9600);
   pinMode(MA1, OUTPUT);
@@ -319,20 +314,20 @@ void setup()
 
   pinMode(ECHO, INPUT);
   pinMode(TRIG, OUTPUT);
-  
-  gripper(GRIPPER_OPEN, 3); 
-  // Initialize and calibrate
-  allStop();    // Stop motors before calibration
-  // calibrate();  // Set _black color threshold
 
-  start(); 
+  gripper(GRIPPER_OPEN, 3);
+  // Initialize and calibrate
+  allStop();  // Stop motors before calibration
+  calibrate();  // Set _black color threshold
+
+  start();
   setMotors(255, 255);
 }
 
 void loop()
 {
   // Main line following logic
-
+  // Serial.println(getDistance());
   followLine();
-  
+
 }
